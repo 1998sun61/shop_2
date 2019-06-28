a74be8a3a87a41383de6f677628352e35702e190
sun <1771448756@qq.com>
2019-06-28 10:51:53 +0800

    登录界面完成

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false;
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+Vue.config.productionTip = false
+
+import './assets/css/index.css'
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,86 @@
+<template>
+  <el-row type="flex" class="row-bg" justify="center" align="middle">
+    <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6" class="formData">
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+        label-position="top"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="ruleForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="ruleForm.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          {
+            min: 5,
+            max: 12,
+            message: "用户名在 5 到 12 个字符",
+            trigger: "change"
+          }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            min: 6,
+            max: 16,
+            message: "密码在 6 到 16 个字符",
+            trigger: "change"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
+
+<style>
+.row-bg {
+  height: 100%;
+  background-color: #ccc;
+}
+.formData {
+  background-color: #fff;
+  padding: 30px 20px;
+  min-width: 400px;
+  border-radius: 10px;
+}
+</style>
